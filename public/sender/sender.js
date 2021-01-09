ff5cb5d4135eda37aafe8b20b8066d542adda44a
amansingh0301 <as9997450@gmail.com>
2021-01-10 04:58:56 +0530

    added turn

--- public/sender/sender.js
+++ public/sender/sender.js
@@ -205,6 +205,9 @@ function joinCall() {
                     "urls": ["stun:stun.l.google.com:19302", 
                     "stun:stun1.l.google.com:19302", 
                     "stun:stun2.l.google.com:19302"]
+                },
+                {
+                    "urls":"turn:numb.viagenie.ca", "username":"webrtc@live.com", "credential":"muazkh"
                 }
             ]
         }
