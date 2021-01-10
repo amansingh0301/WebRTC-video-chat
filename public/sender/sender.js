15f715d92fa25a45c92a0b724d4a2be50109b30e
amansingh0301 <as9997450@gmail.com>
2021-01-10 10:55:44 +0530

    fixed playing bug

--- public/sender/sender.js
+++ public/sender/sender.js
@@ -2,9 +2,9 @@
 var HOST = location.origin.replace(/^http/, 'ws')
 var webSocket = new WebSocket(HOST);
 
-document.getElementById("local-video").autoplay = true;
-document.getElementById("local-video").playsInline = true;
-document.getElementById("local-video").muted = true;
+document.getElementById("remote-video").autoplay = true;
+document.getElementById("remote-video").playsInline = true;
+document.getElementById("remote-video").muted = true;
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
