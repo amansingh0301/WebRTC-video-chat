8ff6ffaffb53103f630052c934e68c959daccd47
amansingh0301 <as9997450@gmail.com>
2021-02-21 14:06:08 +0530

    stun server added

--- public/sender/sender.js
+++ public/sender/sender.js
@@ -1,170 +1,159 @@
-var HOST = location.origin.replace(/^http/, 'ws')
+var HOST = location.origin.replace(/^http/, "ws");
 var webSocket = new WebSocket(HOST);
 
 document.getElementById("local-video").autoplay = true;
-// document.getElementById("local-video").playsinline = true;
 document.getElementById("local-video").muted = true;
 
-
 webSocket.onmessage = (event) => {
-    handleSignallingData(JSON.parse(event.data))
-}
+  handleSignallingData(JSON.parse(event.data));
+};
 
 function handleSignallingData(data) {
-    switch (data.type) {
-        case "answer":
-            peerConn.setRemoteDescription(data.answer)
-            break
-        case "offer":
-            peerConnj.setRemoteDescription(data.offer)
-            .then(() => {
-                createAndSendAnswerj()
-            })
-            break
-        case "candidate":
-            if(data.from == 'receiver')
-                peerConn.addIceCandidate(data.candidate)
-            else
-                peerConnj.addIceCandidate(data.candidate)
-    }
+  switch (data.type) {
+    case "answer":
+      peerConn.setRemoteDescription(data.answer);
+      break;
+    case "offer":
+      peerConnj.setRemoteDescription(data.offer).then(() => {
+        createAndSendAnswerj();
+      });
+      break;
+    case "candidate":
+      if (data.from == "receiver") peerConn.addIceCandidate(data.candidate);
+      else peerConnj.addIceCandidate(data.candidate);
+  }
 }
 
 function sendData(data) {
-    data.username = username
-    data.from = 'sender'
-    webSocket.send(JSON.stringify(data))
+  data.username = username;
+  data.from = "sender";
+  webSocket.send(JSON.stringify(data));
 }
 
-let localStream
-let peerConn
-let username
+let localStream;
+let peerConn;
+let username;
 
 /*
 This 'startCall' function takes the user ID of the person who starts the call.
 And this function is soul function require this function in another module and call it, then it starts a call with user ID as room name.
 */
 function startCall() {
-    username = document.getElementById('username-input').value;
-    sendData({
-        type: "store_user"
+  username = document.getElementById("username-input").value;
+  sendData({
+    type: "store_user",
+  });
+  document.getElementById("video-call-div").style.display = "inline";
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
     })
-    document.getElementById("video-call-div")
-    .style.display = "inline"
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-    })
-    .then((stream) => {
-        localStream = stream
-        console.log('here');
-        document.getElementById("local-video").srcObject = localStream
+    .then(
+      (stream) => {
+        localStream = stream;
+        console.log("here");
+        document.getElementById("local-video").srcObject = localStream;
 
         let configuration = {
-            iceServers: [
-                {
-                    "urls": ["stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302", 
-                     "stun:stun2.l.google.com:19302"]
-                },
-                {
-                    "urls": [
-                    "turn:numb.viagenie.ca"
-                    ],
-                    "username": "webrtc@live.com",
-                    "credential": "muazkh"
-                    
-                    // "urls": [
-                    //     "turn:64.233.165.127:19305?transport=udp",
-                    //     "turn:[2A00:1450:4010:C01::7F]:19305?transport=udp"
-                    //     ]
-                }
-            ]
-        }
+          iceServers: [
+            {
+              urls: [
+                "stun:stun.stunprotocol.org",
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+              ],
+            },
+            {
+              urls: ["turn:numb.viagenie.ca"],
+              username: "webrtc@live.com",
+              credential: "muazkh",
+            },
+          ],
+        };
 
-        peerConn = new RTCPeerConnection(configuration)
-        localStream.getTracks().forEach(function(track) {
-            peerConn.addTrack(track, localStream);
+        peerConn = new RTCPeerConnection(configuration);
+        localStream.getTracks().forEach(function (track) {
+          peerConn.addTrack(track, localStream);
+        });
+
+        peerConn.onconnectionstatechange = (e) => {
+          console.log(peerConn.connectionState);
+        };
+
+        const remoteStream = new MediaStream();
+        const remoteVideo = document.getElementById("remote-video");
+        remoteVideo.srcObject = remoteStream;
+        remoteVideo.playsinline = true;
+        remoteVideo.autoplay = true;
+
+        peerConn.addEventListener("track", async (event) => {
+          remoteStream.addTrack(event.track, remoteStream);
+          console.log("video received");
+        });
+
+        peerConn.onicecandidate = (e) => {
+          if (e.candidate == null) return;
+          console.log("new candidate : ", e.candidate);
+          sendData({
+            type: "store_candidate",
+            candidate: e.candidate,
           });
+        };
 
-          peerConn.onconnectionstatechange = (e) => {
-              console.log(peerConn.connectionState)
-          }
-
-          const remoteStream = new MediaStream();
-            const remoteVideo = document.getElementById("remote-video");
-            remoteVideo.srcObject = remoteStream;
-            remoteVideo.playsinline=true;
-            remoteVideo.autoplay=true;
-
-            peerConn.addEventListener('track', async (event) => {
-                remoteStream.addTrack(event.track, remoteStream);
-                console.log('video received')
-            });
-
-        peerConn.onicecandidate = ((e) => {
-            if (e.candidate == null)
-                return
-            console.log('new candidate : ',e.candidate)
-            sendData({
-                type: "store_candidate",
-                candidate: e.candidate
-            })
-        })
-
-        createAndSendOffer()
-    },(error) => {
-        console.log(error)
-    })
+        createAndSendOffer();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 }
 
 function createAndSendOffer() {
-    console.log('creating Offer...')
-    peerConn.createOffer()
-    .then(offer => 
-        {
-            console.log('Setting LocalDescription...')
-            peerConn.setLocalDescription(offer)
-            .then(() => {
-                console.log('Offer sent : ',offer)
-                sendData({
-                    type: "store_offer",
-                    offer: offer
-                })
-            })
-            
-        })
-            
-    .catch((error) => {
-        console.log(error)
+  console.log("creating Offer...");
+  peerConn
+    .createOffer()
+    .then((offer) => {
+      console.log("Setting LocalDescription...");
+      peerConn.setLocalDescription(offer).then(() => {
+        console.log("Offer sent : ", offer);
+        sendData({
+          type: "store_offer",
+          offer: offer,
+        });
+      });
     })
+
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 /*
 require these two functions and mute and unmute video and audio 
 */
 
-var isAudio=true;
+var isAudio = true;
 function muteAudio() {
-    isAudio = !isAudio
-    localStream.getAudioTracks()[0].enabled = isAudio
-    if(isAudio==true)
-        document.getElementById('audio-btn').textContent = 'Mute Audio'
-    else
-        document.getElementById('audio-btn').textContent = 'Unmute Audio'
+  isAudio = !isAudio;
+  localStream.getAudioTracks()[0].enabled = isAudio;
+  if (isAudio == true)
+    document.getElementById("audio-btn").textContent = "Mute Audio";
+  else document.getElementById("audio-btn").textContent = "Unmute Audio";
 }
 
-var isVideo=true;
+var isVideo = true;
 function muteVideo() {
-    isVideo = !isVideo
-    localStream.getVideoTracks()[0].enabled = isVideo
-    if(isVideo==true)
-        document.getElementById('video-btn').textContent = 'Mute Video'
-    else
-        document.getElementById('video-btn').textContent = 'Unmute Video'
+  isVideo = !isVideo;
+  localStream.getVideoTracks()[0].enabled = isVideo;
+  if (isVideo == true)
+    document.getElementById("video-btn").textContent = "Mute Video";
+  else document.getElementById("video-btn").textContent = "Unmute Video";
 }
 
-let peerConnj
-let usernamej
+let peerConnj;
+let usernamej;
 
 /*
 This 'joinCall' function takes the user ID of the person who starts the call.
@@ -172,130 +161,96 @@ And this function is soul function. Require this function in another module and 
 */
 
 function joinCall() {
-    usernamej = document.getElementById('username-input').value;
-    document.getElementById("video-call-div")
-    .style.display = "inline"
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
+  usernamej = document.getElementById("username-input").value;
+  document.getElementById("video-call-div").style.display = "inline";
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
     })
-    .then((stream) => {
-        localStream = stream
-        document.getElementById("local-video").srcObject = localStream
+    .then(
+      (stream) => {
+        localStream = stream;
+        document.getElementById("local-video").srcObject = localStream;
 
         let configuration = {
-            iceServers: [
-                // {
-                //     'urls': 'stun:stun.l.google.com:19302'
-                //   },
-                //   {
-                //     'urls': 'turn:192.158.29.39:3478?transport=udp',
-                //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                //     'username': '28224511:1379330808'
-                //   },
-                //   {
-                //     'urls': 'turn:192.158.29.39:3478?transport=tcp',
-                //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                //     'username': '28224511:1379330808'
-                //   }
-                // , 
-                //     "stun:stun1.l.google.com:19302", 
-                //     "stun:stun2.l.google.com:19302"
-                {
-                    "urls": ["stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302", 
-                     "stun:stun2.l.google.com:19302"]
-                },
-                {
-                    "urls": [
-                        "turn:numb.viagenie.ca"
-                        ],
-                        "username": "webrtc@live.com",
-                        "credential": "muazkh"
-                    // "urls": [
-                    // "turn:13.250.13.83:3478?transport=udp"
-                    // ],
-                    // "username": "YzYNCouZM1mhqhmseWk6",
-                    // "credential": "YzYNCouZM1mhqhmseWk6"
-                    // "urls": [
-                    //     "turn:webrtcweb.com:7788"
-                    //     ],
-                    //     "username": "muazkh",
-                    //     "credential": "muazkh"
-                    // "urls": [
-                    // "turn:64.233.165.127:19305?transport=udp",
-                    // "turn:[2A00:1450:4010:C01::7F]:19305?transport=udp"
-                    // ]
-                }
-            ]
-        }
+          iceServers: [
+            {
+              urls: [
+                "stun:stun.stunprotocol.org",
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+              ],
+            },
+            {
+              urls: ["turn:numb.viagenie.ca"],
+              username: "webrtc@live.com",
+              credential: "muazkh",
+            },
+          ],
+        };
 
-        peerConnj = new RTCPeerConnection(configuration)
-        localStream.getTracks().forEach(function(track) {
-            peerConnj.addTrack(track, localStream);
+        peerConnj = new RTCPeerConnection(configuration);
+        localStream.getTracks().forEach(function (track) {
+          peerConnj.addTrack(track, localStream);
+        });
+        peerConnj.onconnectionstatechange = (e) => {
+          console.log(peerConnj.connectionState);
+        };
+
+        const remoteStream = new MediaStream();
+        const remoteVideo = document.getElementById("remote-video");
+        remoteVideo.srcObject = remoteStream;
+        remoteVideo.playsinline = true;
+        remoteVideo.autoplay = true;
+
+        peerConnj.addEventListener("track", async (event) => {
+          remoteStream.addTrack(event.track, remoteStream);
+          console.log("video received");
+        });
+
+        peerConnj.onicecandidate = (e) => {
+          if (e.candidate == null) return;
+          console.log("new candidate : ", e.candidate);
+
+          sendDataj({
+            type: "send_candidate",
+            candidate: e.candidate,
           });
-          peerConnj.onconnectionstatechange = (e) => {
-            console.log(peerConnj.connectionState)
-        }
-
-            const remoteStream = new MediaStream();
-            const remoteVideo = document.getElementById("remote-video");
-            remoteVideo.srcObject = remoteStream;
-            remoteVideo.playsinline=true;
-            remoteVideo.autoplay=true;
-
-            peerConnj.addEventListener('track', async (event) => {
-                remoteStream.addTrack(event.track, remoteStream);
-                console.log('video received')
-            });
-
-        // peerConnj.ontrack = (e) => {
-        //     // document.getElementById("remote-video").srcObject=e.streams[0]
-            
-        //         remoteStream.addTrack(e.streams[0], remoteStream);
-        //         console.log(e.streams[0])
-            
-        // }
-
-        peerConnj.onicecandidate = ((e) => {
-            if (e.candidate == null)
-                return
-        console.log('new candidate : ',e.candidate)
-
-            sendDataj({
-                type: "send_candidate",
-                candidate: e.candidate
-            })
-        })
+        };
 
         sendDataj({
-            type: "join_call"
-        })
-
-    },(error) => {
-        console.log('inside : ',error);
-    })
+          type: "join_call",
+        });
+      },
+      (error) => {
+        console.log("inside : ", error);
+      }
+    );
 }
 
-function createAndSendAnswerj () {
-    console.log('creating answer...')
-    peerConnj.createAnswer((answer) => {
-        console.log('setting LocalDescription...')
-        peerConnj.setLocalDescription(answer)
-        .then(() => {
-            console.log('Answer sent : ',answer);
-            sendDataj({
-                type: "send_answer",
-                answer: answer
-            })
-        })
-    }, error => {
-        console.log(error)
-    })
+function createAndSendAnswerj() {
+  console.log("creating answer...");
+  peerConnj.createAnswer(
+    (answer) => {
+      console.log("setting LocalDescription...");
+      peerConnj.setLocalDescription(answer).then(() => {
+        console.log("Answer sent : ", answer);
+        sendDataj({
+          type: "send_answer",
+          answer: answer,
+        });
+      });
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 }
 
 function sendDataj(data) {
-    data.username = usernamej
-    data.from = 'receiver'
-    webSocket.send(JSON.stringify(data))
+  data.username = usernamej;
+  data.from = "receiver";
+  webSocket.send(JSON.stringify(data));
 }
